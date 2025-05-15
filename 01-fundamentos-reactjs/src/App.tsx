378177b4757ced19170ffdca5bc4ffec656b3418
Marcelllombm <marceloborgesdemelo@macminimarcelllombm.local>
2025-05-15 05:51:38 -0300

    style: styled header

--- 01-fundamentos-reactjs/src/App.tsx
+++ 01-fundamentos-reactjs/src/App.tsx
@@ -1,12 +1,18 @@
 import { Header } from "./components/Header";
 import { TodoList } from "./components/TodoList";
+import rocket from "./assets/rocket.svg";
 
 import "./global.css";
 
 export function App() {
   return (
     <>
-      <Header />
+      <Header
+        titleH1="todo"
+        src={rocket}
+        alt="Imagem de um Foguete"
+        title="Foguete"
+      />
       <TodoList />
     </>
   );
--- 01-fundamentos-reactjs/src/components/Header.tsx
+++ 01-fundamentos-reactjs/src/components/Header.tsx
@@ -1,3 +1,32 @@
-export function Header() {
-  return "Header";
+import type { ImgHTMLAttributes } from "react";
+import styles from "./Header.module.css";
+
+interface HeaderProps extends ImgHTMLAttributes<HTMLImageElement> {
+  titleH1: string;
+}
+
+export function Header({ titleH1, ...props }: HeaderProps) {
+  let styledTitle;
+
+  if (titleH1.length >= 4) {
+    const middle = Math.floor(titleH1.length / 2);
+    const firstHalf = titleH1.slice(0, middle);
+    const secondHalf = titleH1.slice(middle);
+
+    styledTitle = (
+      <h1>
+        <span className={styles.spanBlue}>{firstHalf}</span>
+        <span className={styles.spanPurpleDark}>{secondHalf}</span>
+      </h1>
+    );
+  } else {
+    styledTitle = <h1>titleH1</h1>;
+  }
+
+  return (
+    <header className={styles.header}>
+      <img {...props} />
+      <h1>{styledTitle}</h1>
+    </header>
+  );
 }
