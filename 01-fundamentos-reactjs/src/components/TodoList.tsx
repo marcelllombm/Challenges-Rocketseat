66255b9939f48ecdb0853b9a18d4332e7d00a7cc
Marcelllombm <marceloborgesdemelo@macminimarcelllombm.local>
2025-05-15 07:09:08 -0300

    feat(TodoList): component structure

--- 01-fundamentos-reactjs/src/components/TodoList.tsx
+++ 01-fundamentos-reactjs/src/components/TodoList.tsx
@@ -1,3 +1,25 @@
+import { PlusCircle, ClipboardText } from "@phosphor-icons/react";
+
 export function TodoList() {
-  return "List";
+  return (
+    <main>
+      <form>
+        <input />
+        <button type="submit">
+          Criar <PlusCircle />
+        </button>
+      </form>
+
+      <div>
+        <span>Tarefas</span>
+        <span>Concluidas</span>
+      </div>
+
+      <div>
+        <ClipboardText />
+        <p>Você ainda não tem tarefas cadastradas</p>
+        <p>Crie tarefas e organize seu itens a fazer</p>
+      </div>
+    </main>
+  );
 }
