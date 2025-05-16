ddbf00fb841318794ca0098547a6f78e0c32d38b
Marcelllombm <marceloborgesdemelo@macminimarcelllombm.local>
2025-05-16 18:54:40 -0300

    refactor(TodoList): adding feature to the basic structure of the component

--- 01-fundamentos-reactjs/src/components/TodoList.tsx
+++ 01-fundamentos-reactjs/src/components/TodoList.tsx
@@ -2,6 +2,14 @@ import { PlusCircle, ClipboardText } from "@phosphor-icons/react";
 import styles from "./TodoList.module.css";
 
 export function TodoList() {
+  const zeroListing = (
+    <>
+      <ClipboardText size={56} weight="light" color="#333333" />
+      <strong>Você ainda não tem tarefas cadastradas</strong>
+      <p>Crie tarefas e organize seu itens a fazer</p>
+    </>
+  );
+
   return (
     <main className={styles.container}>
       <form className={styles.form}>
@@ -10,16 +18,17 @@ export function TodoList() {
           Criar <PlusCircle className={styles.plusCircle} size={20} />
         </button>
       </form>
+      <div className={styles.content}>
+        <div className={styles.summary}>
+          <div className={styles.task}>
+            Tarefas criadas <span>0</span>
+          </div>
+          <div className={styles.completed}>
+            Concluídas <span>0</span>
+          </div>
+        </div>
 
-      <div>
-        <span>Tarefas</span>
-        <span>Concluidas</span>
-      </div>
-
-      <div>
-        <ClipboardText />
-        <p>Você ainda não tem tarefas cadastradas</p>
-        <p>Crie tarefas e organize seu itens a fazer</p>
+        <div className={styles.list}>{zeroListing}</div>
       </div>
     </main>
   );
